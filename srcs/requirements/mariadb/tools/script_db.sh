3d424aff00a01b42e0e435792fa5d5600a4650ab
HChairiRuya <hchairi1902@gmail.com>
2024-04-30 14:12:23 -0400

    connected

--- srcs/requirements/mariadb/tools/script_db.sh
+++ srcs/requirements/mariadb/tools/script_db.sh
@@ -6,10 +6,10 @@ service mariadb start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`TEST_DATABASE\`;"
 
 # Créer l'utilisateur si elle n'existe pas déjà
-mysql -e "CREATE USER IF NOT EXISTS \`TEST_USER\`@'localhost' IDENTIFIED BY 'TEST_PASS';"
+mysql -e "CREATE USER IF NOT EXISTS \`TEST_USER\`@'%' IDENTIFIED BY 'TEST_PASS';"
 
 # Accorder tous les privilèges à l'utilisateur sur la base de données
-mysql -e "GRANT ALL PRIVILEGES ON \`TEST_DATABASE\`.* TO 'TEST_USER'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'TEST_USER'@'%';"
 
 # Appliquer les changements
 mysql -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/setup_wp_config.sh
+++ srcs/requirements/wordpress/tools/setup_wp_config.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Attendre que la base de données MariaDB soit prête
-sleep 2
+# # Attendre que la base de données MariaDB soit prête
+sleep 5
 
-# Vérifier si le fichier wp-config.php existe déjà
+# # Vérifier si le fichier wp-config.php existe déjà
 if [ ! -f "wp-config.php" ]; then
-    # Créer le fichier de configuration wp-config.php
-    wp config create \
-    --dbname=TEST_DATABASE \
-    --dbuser=TEST_USER \
-    --dbpass=TEST_PASS \
-    --dbhost=mariadb \
-    --dbprefix=wp_ --skip-check
+#     # Créer le fichier de configuration wp-config.php
+    wp config create	--allow-root \
+        --dbname="TEST_DATABASE" \
+        --dbuser="TEST_USER" \
+        --dbpass="TEST_PASS" \
+        --dbhost="172.17.0.2" --path='/var/www/wordpress'
+					
 fi
-
-exec "$@" 
+exec "$@"
