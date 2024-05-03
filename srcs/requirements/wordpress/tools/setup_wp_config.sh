51b88d33a2ec039aee4ffd3f9989b4b790942caa
HChairiRuya <hchairi1902@gmail.com>
2024-05-03 15:45:14 -0400

    wordpress

--- srcs/requirements/wordpress/tools/setup_wp_config.sh
+++ srcs/requirements/wordpress/tools/setup_wp_config.sh
@@ -1,16 +1,37 @@
-# #!/bin/bash
+# # #!/bin/bash
 
-# # Attendre que la base de données MariaDB soit prête
-sleep 5
+# # # Vérifier si le fichier wp-config.php existe déjà
+# if [ ! -f "wp-config.php" ]; then
+#     echo "Creating wp-config.php..."
+# #     # Créer le fichier de configuration wp-config.php
+#     # wp config create	--allow-root \
+#     #     --dbname="TEST_DATABASE" \
+#     #     --dbuser="TEST_USER" \
+#     #     --dbpass="TEST_PASS" \
+#     #     --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
+#     # sleep 5
+# 	wp config create --allow-root \
+#         --dbname="TEST_DATABASE" \
+#         --dbuser="TEST_USER" \
+#         --dbpass="TEST_PASS" \
+#         --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
+					
+# fi
+# exec "$@"
 
-# # Vérifier si le fichier wp-config.php existe déjà
-if [ ! -f "wp-config.php" ]; then
-#     # Créer le fichier de configuration wp-config.php
-    wp config create	--allow-root \
+#!/bin/sh
+
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+	sleep 5
+	wp config create --allow-root \
         --dbname="TEST_DATABASE" \
         --dbuser="TEST_USER" \
         --dbpass="TEST_PASS" \
-        --dbhost="172.17.0.2" --path='/var/www/wordpress'
-					
+        --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
 fi
+
 exec "$@"
