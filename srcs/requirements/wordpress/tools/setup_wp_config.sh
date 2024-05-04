d9408e0a78348b268fa8f2d2d2af7751b6e919b4
HChairiRuya <hchairi1902@gmail.com>
2024-05-04 14:23:02 -0400

    Fix containers bugs

--- srcs/requirements/wordpress/tools/setup_wp_config.sh
+++ srcs/requirements/wordpress/tools/setup_wp_config.sh
@@ -1,37 +1,32 @@
-# # #!/bin/bash
+# #!/bin/bash
 
-# # # Vérifier si le fichier wp-config.php existe déjà
-# if [ ! -f "wp-config.php" ]; then
-#     echo "Creating wp-config.php..."
-# #     # Créer le fichier de configuration wp-config.php
-#     # wp config create	--allow-root \
-#     #     --dbname="TEST_DATABASE" \
-#     #     --dbuser="TEST_USER" \
-#     #     --dbpass="TEST_PASS" \
-#     #     --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
-#     # sleep 5
+# Vérifier si le fichier wp-config.php existe déjà
+if [ ! -f "wp-config.php" ]; then
+    echo "Creating wp-config.php..."
+# Créer le fichier de configuration wp-config.php
+	wp config create --allow-root \
+        --dbname="TEST_DATABASE" \
+        --dbuser="TEST_USER" \
+        --dbpass="TEST_PASS" \
+        --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'				
+fi
+exec "$@"
+
+#!/bin/sh
+
+#check if wp-config.php exist
+# if [ -f ./wp-config.php ]
+# then
+# 	echo "wordpress already downloaded"
+# else
+# 	sleep 5
 # 	wp config create --allow-root \
 #         --dbname="TEST_DATABASE" \
 #         --dbuser="TEST_USER" \
 #         --dbpass="TEST_PASS" \
 #         --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
-					
+
+
 # fi
+
 # exec "$@"
-
-#!/bin/sh
-
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-	sleep 5
-	wp config create --allow-root \
-        --dbname="TEST_DATABASE" \
-        --dbuser="TEST_USER" \
-        --dbpass="TEST_PASS" \
-        --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
-fi
-
-exec "$@"
