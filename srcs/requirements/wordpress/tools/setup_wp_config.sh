0680bb75989fe7dbdf19641c166240d996fddc18
HChairiRuya <hchairi1902@gmail.com>
2024-05-05 12:13:04 -0400

    maraidb service in docker-compose

--- srcs/requirements/wordpress/tools/setup_wp_config.sh
+++ srcs/requirements/wordpress/tools/setup_wp_config.sh
@@ -8,7 +8,20 @@ if [ ! -f "wp-config.php" ]; then
         --dbname="TEST_DATABASE" \
         --dbuser="TEST_USER" \
         --dbpass="TEST_PASS" \
-        --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'				
+        --dbhost="172.17.0.2:3306" --path='/var/www/wordpress'
+
+        # ---- Do tomorroww
+        # wp core install --allow-root \
+        # --url=$URL \
+        # --title=$TITLE \
+        # --admin_user=$WP_ADMIN_USER \
+        # --admin_password=$WP_ADMIN_PASS \
+        # --admin_email=$WP_ADMIN_EMAIL
+
+        # wp user create --allow-root \
+        # $WP_SECOND_USER \
+        # $WP_SECOND_USER_MAIL \
+        # --user_pass=$WP_SECOND_USER_PASS
 fi
 exec "$@"
 
